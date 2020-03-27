54604bee2b7bdf2dd6ecd281e3977bef7c32fcde
Abdullah Qureshi <iamabqureshi47@gmail.com>
2020-03-27 22:56:20 +0545

    feat: background added in the home page

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { styled } from "styletron-react";
+import theme from "../../theme";
 
 const Container = styled("div", () => ({
   height: "100vh",
@@ -7,20 +8,31 @@ const Container = styled("div", () => ({
   backgroundColor: "#32a852",
   display: "flex",
   alignItems: "center",
-  justifyContent: "center"
+  justifyContent: "center",
+  backgroundAttachment: "fixed",
+  backgroundImage: "url('./assets/home-bg.jpg')",
+  backgroundSize: "cover",
+  backgroundPosition: "center",
 }));
 
 const Button = styled("button", () => ({
   padding: "2rem",
   fontSize: "2rem",
-  borderRadius: "6px"
+  borderRadius: "5rem",
+  backgroundColor: theme.primaryColor,
+  color: "white",
+  outline: "none",
+  transition: "0.3s transform",
+  ":hover": {
+    transform: "translateY(-0.4rem)",
+  },
 }));
 
 export default ({ history }) => {
   return (
     <Container>
       <Button onClick={() => history.push("/notes")}>
-        Start your notes 🙂
+        Start taking notes 🙂
       </Button>
     </Container>
   );
